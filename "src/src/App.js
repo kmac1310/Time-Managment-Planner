2f4bb77ff577e71b990cf9c5534345b57705b042
kmac1310 <mac9136@gmail.com>
2024-08-11 16:09:03 -0400

    Create App.js

---
+++ "src/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TimeManagementPlanner from './TimeManagementPlanner';
+
+function App() {
+  return (
+    <div className="App">
+      <TimeManagementPlanner />
+    </div>
+  );
+}
+
+export default App;
